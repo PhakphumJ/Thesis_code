d8acb765062a6590e0c9be02833750c347b32671
PhakphumJ <fphakphum.134@gmail.com>
2024-05-05 22:11:50 +0200

    Try out model 2.

--- Model2_Function.m
+++ Model2_Function.m
@@ -2,7 +2,7 @@
 % L_a -> x(1), W_a -> x(2), W_m -> x(3)
 %%%%%%
 
-function F = Model1_Function(x, Z_lt, Z_tt, Z_mt, Mu, Gamma, Hbar_t, SD, P_t, T)
+function F = Model2_Function(x, Z_lt, Z_tt, Z_mt, Mu, Gamma, Hbar_t, SD, P_t, T)
     F(1) = x(2) - P_t*Z_lt*Gamma*(Gamma + (1 - Gamma)*(Z_tt*T/(Z_lt*x(1))...
         )^((Mu-1)/Mu))^(1/(Mu - 1)); % wage a
     F(2) = x(3) - Z_mt; % wage m
--- Model2_OnePeriod_Try.m
+++ Model2_OnePeriod_Try.m
@@ -1,17 +1,20 @@
 cd 'D:\OneDrive - Central European University\CEU\Thesis\Thesis_code'
 
 clear
+
 %% Declare parameters values
-% Parameters needed are (Z_at, Z_mt, alpha, Hbar_t, SD, P_t, T) %
+% Parameters needed are (Z_lt, Z_tt, Z_mt, Gamma, Mu, Hbar_t, SD, P_t, T) %
 
 % Production parameters
-Z_at = 10;
+Z_lt = 20;
+Z_tt = 10;
 Z_mt = 50;
-alpha = 0.3;
-T = 1000; % Land
+Gamma = 0.5;
+Mu = 0.7;
+T = 10; % Land
 
 %Human Capital parameters
-Hbar_t = 5;
+Hbar_t = 2;
 SD = 1;
 
 % Price
@@ -21,5 +24,4 @@ P_t = 5;
 % Description: % L_a -> x(1), W_a -> x(2), W_m -> x(3)
 x0 = [0.5; 20; 2];
 
-x_star = fsolve(@(x)Model1_Function(x, Z_at, Z_mt, alpha, Hbar_t, SD, P_t, T), x0);
-
+x_star = fsolve(@(x)Model2_Function(x, Z_lt, Z_tt, Z_mt, Mu, Gamma, Hbar_t, SD, P_t, T), x0);
